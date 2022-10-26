18b6ee2612309032cebf228b9c4af3332caca9a4
ayeasin14 <ayeasin14@gmail.com>
2022-10-27 01:20:37 +0600

    course params added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const course = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Courses API Running');
@@ -16,6 +17,17 @@ app.get('/course-categories', (req, res) => {
     res.send(categories);
 });
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_course = course.filter(c => c.category_id === id);
+    res.send(category_course);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c._id === id);
+    res.send(selectedCourse);
+})
 
 app.listen(port, () => {
     console.log('Courses Server running on port', port);
