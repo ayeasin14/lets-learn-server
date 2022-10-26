9818d9ad4d2bf33b3c77cb7dbfd9653a4c29f0cb
ayeasin14 <ayeasin14@gmail.com>
2022-10-27 00:58:52 +0600

    categories added

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-
+const categories = require('./data/categories.json');
 
 app.get('/', (req, res) => {
     res.send('Courses API Running');
 });
 
 
-
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+});
 
 
 app.listen(port, () => {
