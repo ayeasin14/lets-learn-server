90501181ad0225f6ee9fc100c1cda15a67a7d30e
ayeasin14 <ayeasin14@gmail.com>
2022-10-27 00:54:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Courses API Running');
+});
+
+
+
+
+
+app.listen(port, () => {
+    console.log('Courses Server running on port', port);
+})
